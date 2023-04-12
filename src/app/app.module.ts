7ff144d393a50d96ac93ab7cf2475b994adb86ba
Joas <joaoferreirapaula005@gmail.com>
2023-04-12 17:31:39 -0300

    Primeiro commit - aprendendo angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { TweetWrapperComponent } from './components/tweet-wrapper/tweet-wrapper.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    TweetWrapperComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/tweet-wrapper/tweet-wrapper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tweet-wrapper',
+  templateUrl: './tweet-wrapper.component.html',
+  styleUrls: ['./tweet-wrapper.component.css']
+})
+export class TweetWrapperComponent {
+
+}
